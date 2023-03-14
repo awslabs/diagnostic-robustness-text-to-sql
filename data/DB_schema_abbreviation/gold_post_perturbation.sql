0d5f22a3a4f57d5dacc5f28ad9c2c4a7a9d3b087
Shuaichen Chang <shuaichenchang@users.noreply.github.com>
2023-03-13 22:14:16 -0400

    fix eval errors caused by self-join queries and text-type numbers

--- data/DB_schema_abbreviation/gold_post_perturbation.sql
+++ data/DB_schema_abbreviation/gold_post_perturbation.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f0705c49a1329a2a964ea34d0bf00ea4bc5d2c4b18c72f0f2bc68bca803e999
-size 420725
+oid sha256:005e5d897315dffc0434ddbb4e5dd81f1654eb99f6a14d36c0c94d932585c167
+size 419289
--- data/DB_schema_synonym/gold_post_perturbation.sql
+++ data/DB_schema_synonym/gold_post_perturbation.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c232824ecaebd426d43b7724a6c2e596fd42ef0b7b032934e76b8b204938fe5
-size 415715
+oid sha256:85a0e3f94b6129476a22ca89e2997aacee9c2da92dcf15613c0f547fd141ee1a
+size 414581
